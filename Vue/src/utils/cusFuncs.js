4840aba8626092eef1ae75a6c827ad81f15d8395
Aditya-Invento <adi.invento@gmail.com>
2023-05-25 20:56:06 +0530

    Production Completed | index.html remaining

--- Vue/src/utils/cusFuncs.js
+++
@@ -1,8 +0,0 @@
-export const def_prop = (type,def='',required=false,validator=()=>{}) => {
-  return {
-    type: type,
-    required: required,
-    default: def,
-    validator: validator//true = valid
-  }
-}
