81b2f59cf6adbd6d06f511fcdb1a5bf71117d1b6
Aditya Burman <122102769+Aditya-Invento@users.noreply.github.com>
2023-06-20 22:41:08 +0530

    Update index.js 2

--- index.js
+++ index.js
@@ -21,25 +21,19 @@ app.use(cookie('super_secret', {
   path: '/'
 }));
 app.use(request_ip.mw());
-app.use(express.static("public"));
-app.use(express.json());
-// All middlewares
-
-app.use('/weather', weather_router);
-
 // Catch-all route for all other requests
 app.get('*', (req, res, next) => {
-  // Exclude requests for static files
-  if (req.url.startsWith('/public/')) {
-    return next();
-  }
-
   res.status(503).json({
     site: 'Weather App',
     msg: 'Site Under Construction 🚧',
     err: 503
   });
 });
+app.use(express.static("public"));
+app.use(express.json());
+// All middlewares
+
+app.use('/weather', weather_router);
 
 app.listen(process.env.PORT || 3000, err => {
   if (err) throw err;
